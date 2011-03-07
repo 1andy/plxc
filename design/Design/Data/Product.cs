053bf261a965328e0516878990f11b8f99d022ba
Evgeny T <evgeny.tukin@gmail.com>
2011-03-07 22:18:42 +0200

    CSS

---
+++ design/Design/Data/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Design.Data
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
